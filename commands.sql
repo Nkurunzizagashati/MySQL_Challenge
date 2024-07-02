1f25b362267f4977daecb0dc18303337b2f9da1a
Fabrice <f.nkurunziz@alustudent.com>
2024-07-02 10:30:00 +0200

    Find all projects with a deadline before December 1st, 2024

--- commands.sql
+++ commands.sql
@@ -79,3 +79,4 @@ INSERT INTO TeamMembers (ProjectID, EmployeeID) VALUES
  (5, 6);
 
 --  Find all projects with a deadline before December 1st, 2024
+SELECT * FROM Projects WHERE Deadline < '2024-12-01';
