381a33eef3bff6f8ba3c581c03e4358876d66fdf
Fabrice <f.nkurunziz@alustudent.com>
2024-07-02 10:30:00 +0200

    queries

--- commands.sql
+++ commands.sql
@@ -421,3 +421,14 @@ GROUP BY
     c.client_name;
 
 SELECT * FROM Project_Details_With_Team_Members;
+
+
+--23.Create a view to show overdue projects with the number of days overdue
+CREATE VIEW Overdue_Projects AS
+SELECT p.project_id, p.project_name, p.requirements, p.deadline, p.client_id, c.client_name, DATEDIFF(CURRENT_DATE, p.deadline) AS days_overdue
+FROM project p
+    JOIN client c ON p.client_id = c.client_id
+WHERE
+    p.deadline < CURRENT_DATE;
+
+SELECT * FROM Overdue_Projects;
