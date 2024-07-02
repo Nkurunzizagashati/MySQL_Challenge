85cf359e02584e34bcc7f1ac106989c17b4c871d
Fabrice <f.nkurunziz@alustudent.com>
2024-07-02 10:30:00 +0200

    queries

--- commands.sql
+++ commands.sql
@@ -184,3 +184,13 @@ FROM project
     JOIN client ON project.client_id = client.client_id
 WHERE
     deadline BETWEEN '2024-11-01' AND '2024-11-30';
+
+
+--15. Create a view to display the total number of projects assigned to each employee.
+CREATE OR REPLACE VIEW Employee_Project_Count AS
+SELECT employees.employee_id, employees.employee_name, COUNT(team_members.project_id) AS projects_assigned
+FROM employees
+    JOIN team_members ON team_members.employee_id = employees.employee_id
+GROUP BY
+    employees.employee_id,
+    employees.employee_name;
