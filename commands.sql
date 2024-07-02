047ab9267e1a00431d10c0abc9a86f66f437418f
Fabrice <f.nkurunziz@alustudent.com>
2024-07-02 10:30:00 +0200

    queries

--- commands.sql
+++ commands.sql
@@ -177,9 +177,9 @@ FROM
 
 --14.Create a view to show project names and client contact information for projects with a deadline in November 2024.
 CREATE OR REPLACE VIEW Project_Client_November AS
-SELECT project.project_name, project.deadline, client.contact_name, client.client_name
-FROM project
-    JOIN client ON project.ClientID = client.ClientID
+SELECT projects.project_name, projects.deadline, clients.contact_name, clients.client_name
+FROM projects
+    JOIN clients ON projects.ClientID = clients.ClientID
 WHERE
     deadline BETWEEN '2024-11-01' AND '2024-11-30';
 
