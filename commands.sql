c0728503409e8c27ae9368968639a457eecef68f
Fabrice <f.nkurunziz@alustudent.com>
2024-07-02 10:30:00 +0200

    queries

--- commands.sql
+++ commands.sql
@@ -194,3 +194,29 @@ FROM employees
 GROUP BY
     employees.employee_id,
     employees.employee_name;
+
+
+
+-- 16. Create a function to calculate the number of days remaining until a project deadline.
+DELIMITER $$
+
+CREATE FUNCTION days_until_deadline(project_id INT) 
+RETURNS INT DETERMINISTIC
+BEGIN
+    DECLARE deadline DATE;
+    DECLARE days_remaining INT;
+    
+    -- Get the project deadline
+    SELECT p.deadline INTO deadline
+    FROM project p
+    WHERE p.project_id = project_id;
+
+    -- Calculate the number of days remaining
+    SET days_remaining = DATEDIFF(deadline, CURRENT_DATE);
+    
+    RETURN days_remaining;
+END$$
+
+DELIMITER;
+-- Usage
+SELECT days_until_deadline (1) AS days_remaining;
