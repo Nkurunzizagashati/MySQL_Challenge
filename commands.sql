7cb3ec13ef366b20b611c8ea26b8d2bb1203568f
Fabrice <f.nkurunziz@alustudent.com>
2024-07-02 10:30:00 +0200

    queries

--- commands.sql
+++ commands.sql
@@ -144,3 +144,12 @@ SELECT
         ELSE 'Not overdue'
     END AS status
 FROM project;
+
+
+--11. Create a view to simplify retrieving client contact
+CREATE VIEW ClientContact AS
+SELECT
+    client_id,
+    client_name,
+    contact_name
+FROM client;
